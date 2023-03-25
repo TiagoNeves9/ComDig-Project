0615468cc92ef8b17f95be0d5ee2fa6d9b93038e
TiagoNeves9 <tiagonevesx9@gmail.com>
2023-03-25 22:27:28 +0000

    update

--- Exer1/print_bits.c
+++ Exer1/print_bits.c
@@ -2,7 +2,9 @@
 
 void print_bits(int val) {
     int i;
-    for (i = sizeof(int) * 8 - 1; i >= 0; i--) {
+    for (i = sizeof(int) * 8 - 1; i >= 0; i--) {    //sizeof retorna o valor de int em bytes, 
+                                                    //*8 converte para bits, 
+                                                    //-1 obtemos o indice mais significativo
         putchar((val & (1 << i)) ? '1' : '0');
     }
     putchar('\n');
@@ -11,5 +13,7 @@ void print_bits(int val) {
 int main(){
     print_bits(2);
     print_bits(7);
+    print_bits(-1);
+    print_bits(32);
     return 0;
 }
