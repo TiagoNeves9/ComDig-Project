c80f8f5d7a1e283e4b423c86a568f8c615b9db56
sergio <sergio_e_f_capela@outlook.pt>
2023-05-09 16:08:19 +0100

    testes 2

--- Exer1/ExerC/Exer1_testes.c
+++ Exer1/ExerC/Exer1_testes.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int count_ones( int val );
-void print_bits( int val );
-int count_zeros( int val );
+void print_bits(int val);
+int  count_ones(int val);
+int count_zeros(int val);
 char most_frequent_symbol(char *file_name);
 void negative_file(char *input_file_name, char *output_file_name);
 
@@ -18,13 +18,16 @@ int main (void)
 	char decrypted_en[] = "output/ListaPalavrasENdecrypted.txt";
 
 	print_bits(value);
+
 	printf("Bits with value 0: %dx \n",count_zeros(value));
-	printf("Bits with value 0: %dx \n",count_ones(value));
-    printf("The most frequent symbol in %s :'%c'\n", pt_file, most_freq_symb(pt_file));
-	printf("The most frequent symbol in %s :'%c'\n", en_file, most_freq_symb(en_file));
+	printf("Bits with value 1: %dx \n",count_ones(value));
+
+    printf("Most frequent symbol in %s :'%c'\n", pt_file, most_freq_symb(pt_file));
+	printf("Most frequent symbol in %s :'%c'\n", en_file, most_freq_symb(en_file));
 
 	negative_file(pt_file,encrypted_pt);
 	negative_file(encrypted_pt,decrypted_pt);
+	
 	negative_file(en_file,encrypted_en);
 	negative_file(encrypted_en,decrypted_en);
 
